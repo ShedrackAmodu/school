0e486c42bc6b34864dbeab1937bdaf5df715bde8
ShedrackAmodu <147725659+ShedrackAmodu@users.noreply.github.com>
2025-12-11 18:16:41 +0100

    Update project files and add new migrations

--- staticfiles/js/academics/timetable_form.js
+++
@@ -1,5 +0,0 @@
-// JavaScript for timetable_form.html
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any specific JS for timetable form here
-    // For example, dynamic field visibility based on selections, date pickers, etc.
-});
